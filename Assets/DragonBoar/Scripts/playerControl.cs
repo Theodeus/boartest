6a49c7f811caa95adb4e337d1a2666ca677c0d76
theodeus <oskar.det@gmail.com>
2017-03-12 13:44:50 +0100

    A boar. Walking.

---
+++ Assets/DragonBoar/Scripts/playerControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour 
+{
+	public Animator anim;
+	int scream;
+	int basicAttack;
+	int getHit;
+	int walk;
+	int die;
+	int run;
+	public float Speed = 1f;
+
+
+	void Awake () 
+	{
+		anim = GetComponent<Animator>();
+		scream = Animator.StringToHash("Scream");
+		basicAttack = Animator.StringToHash("Basic Attack");
+		getHit = Animator.StringToHash("Get Hit");
+		walk = Animator.StringToHash("Walk");
+		die = Animator.StringToHash("Die");
+		run = Animator.StringToHash ("Run");
+	}
+
+
+	public void Update()
+	{
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			Speed += 0.01f;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			Speed -= 0.01f;
+		}
+		anim.SetFloat ("Speed", Speed);
+
+		transform.Rotate(Vector3.up, Speed * Time.deltaTime);
+
+		transform.Translate (Vector3.forward * (Speed * Time.deltaTime));
+	}		
+}
